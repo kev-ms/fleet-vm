44cead2968e1271337c5aecfd7da2c45750504b6
bartr <bartr@microsoft.com>
2022-03-24 06:11:52 +0000

    updated pull

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
-
 # pull the latest git updates
 git -C ~/gitops pull
 git -C ~/fleet pull
-
-echo "$(hostname) patched"
