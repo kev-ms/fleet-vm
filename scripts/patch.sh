55fb7bf45e5097af764690e4c6c26cd5865cb8fa
aflinchb <anflinch@microsoft.com>
2022-03-30 20:24:02 +0000

    Patching fluentbit secret

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -5,4 +5,7 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
 docker pull ghcr.io/cse-labs/webv-red:latest
 docker pull ghcr.io/cse-labs/webv-red:beta
 
+# set fluent-bit secret for forwarding to event hub
+kubectl create secret generic fluentbit-eventhub-secret --from-literal="EventHubConnectionString=Endpoint=sb://corp-monitoring-eh.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BVSamLwcySNBamYymanDvKBfrO4r8YjLXgMNgIs7fzw=" -n fluent-bit
+
 echo "$(hostname) patched"
