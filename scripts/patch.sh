fb9dff175601ee0d7e17ae2e29b7abdf9b80a2f6
aflinchb <anflinch@microsoft.com>
2022-03-31 20:19:41 +0000

    fb fix remove

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -5,6 +5,4 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
 docker pull ghcr.io/cse-labs/webv-red:latest
 docker pull ghcr.io/cse-labs/webv-red:beta
 
-kubectl create secret generic fluent-bit-vtlog-secret --from-literal="EventHubConnectionString=Endpoint=sb://corp-monitoring-eh.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BVSamLwcySNBamYymanDvKBfrO4r8YjLXgMNgIs7fzw=" -n vtlog
-
 echo "$(hostname) patched"
