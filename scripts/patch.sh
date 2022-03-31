a20d67eed61107026209418be9ccf9aa0372efd0
aflinchb <anflinch@microsoft.com>
2022-03-31 16:23:33 +0000

    patching fleet

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -5,4 +5,8 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
 docker pull ghcr.io/cse-labs/webv-red:latest
 docker pull ghcr.io/cse-labs/webv-red:beta
 
+kubectl delete secret -n fluent-bit fluentbit-eventhub-secret
+
+kubectl create secret generic fluent-bit-vtlog-secret --from-literal="EventHubConnectionString=Endpoint=sb://corp-monitoring-eh.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BVSamLwcySNBamYymanDvKBfrO4r8YjLXgMNgIs7fzw=" -n vtlog
+
 echo "$(hostname) patched"
